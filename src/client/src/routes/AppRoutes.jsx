fa7599d0bda9ae4ea07dc9e102b56332b621e378
ALfLuisV <alfredoluisvieira@gmail.com>
2024-12-12 12:16:16 -0300

    feat: inicio da implementação da pagina de membros

--- src/client/src/routes/AppRoutes.jsx
+++ src/client/src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Homepage from '../pages/homepage';
+import MembrosPage from '../pages/membros'
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={< Homepage />} />
+            <Route path="/team" element={< MembrosPage />} />
         </Routes>
     );
 }; 
