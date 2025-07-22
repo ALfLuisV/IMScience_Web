e8c45e124f7edce958b96d3bf0125cea9e897dd4
Alfredo Luis Vieira <alfredoluisvieira@gmail.com>
2025-07-21 21:49:27 -0300

    Revert "Delete src/server/Auth/firebaseConfig.js"
    This reverts commit 0f95a233cccfce4b4b38eb1c4f1e9ae3444df3d2.

---
+++ src/server/Auth/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuk3wdh9upGF25uR5PC3Pv_y7hOLKWCe4",
+  authDomain: "imscience-1c1f2.firebaseapp.com",
+  projectId: "imscience-1c1f2",
+  storageBucket: "imscience-1c1f2.firebasestorage.app",
+  messagingSenderId: "441667471584",
+  appId: "1:441667471584:web:f7b732cdf0bd7194dca885"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
